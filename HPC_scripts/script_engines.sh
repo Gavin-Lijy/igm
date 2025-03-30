744303840e603b3ae1517fa63655b099915f1b42
Gavin-Lijy <jingyu.gavin.li@outlook.com>
2025-03-30 22:46:34 +0100

    create

--- HPC_scripts/script_engines.sh
+++ HPC_scripts/script_engines.sh
@@ -9,7 +9,7 @@
 #$ -o out_engines
 #$ -e err_engines
 #$ -V
-#$ -pe dc* 100 
+#$ -pe dc* 24 
 
 
 export PATH="/home/jl9324/miniforge3/envs/igm/bin:/home/jl9324/bin:/home/jl9324/.local/bin:/usr/local/bin:/home/jl9324/env/download/sratoolkit.3.2.1-ubuntu64/bin:/home/jl9324/.vscode-server/cli/servers/Stable-384ff7382de624fb94dbaf6da11977bba1ecd427/server/bin/remote-cli:/home/jl9324/miniforge3/condabin:/home/jl9324/.local/bin:/home/jl9324/bin:/opt/software/spack/bin:/opt/slurm/22.05.8/bin:/opt/slurm/22.05.8/sbin:/usr/share/lmod/lmod/libexec:/usr/bin:/bin:/usr/local/sbin:/usr/sbin"
@@ -21,8 +21,8 @@ cd
 sleep 10
 MONITOR=
 if [[ ! -z "" ]]; then
-    mpirun --n 100 monitor_process --wtype W ipengine
+    mpirun --n 12 monitor_process --wtype W ipengine
 else
-    mpirun --n 100 ipengine
+    mpirun --n 12 ipengine
 fi
 
